25b892dfd143ad5c2e0bf063f3894a4d747e26f8
ashrawan <ashrawan70@gmail.com>
2023-04-18 17:28:25 -0500

    model ts classes

---
+++ src/app/core/model/domain.model.ts
@@ -0,0 +1,74 @@
+enum UserStatus {
+  VERIFIED = 'VERIFIED',
+  UNVERIFIED = 'UNVERIFIED',
+  SUSPENDED = 'SUSPENDED',
+}
+enum EnumRole {
+  ROLE_USER = 'ROLE_USER',
+  ROLE_VENDOR = 'ROLE_VENDOR',
+  ROLE_ADMIN = 'ROLE_ADMIN',
+}
+enum FileType {
+  IMAGES = 'IMAGES',
+  LIST = 'LIST',
+}
+
+export interface UserDTO {
+  userId: number;
+  email: string;
+  password: string;
+  fullName: string;
+  emailVerified?: boolean;
+  phoneNumber?: string;
+  userStatus: UserStatus;
+  role: RoleDTO;
+  address: AddressDTO;
+}
+
+export interface RoleDTO {
+  roleId: number;
+  role: EnumRole;
+}
+
+export interface ProductDTO {
+  productId: number;
+  name: string;
+  description: string;
+  quantity: number;
+  images: FileEntityDTO[];
+
+  isVerified: boolean;
+  isDeleted: boolean;
+
+  vendor: VendorDTO;
+  productCategory: ProductCategoryDTO;
+}
+
+export interface VendorDTO {
+  vendorId: number;
+  description: string;
+  logo: FileEntityDTO;
+
+  user: UserDTO;
+}
+
+export interface ProductCategoryDTO {
+  categoryId: number;
+  category: string;
+}
+
+export interface AddressDTO {
+  addressId: number;
+  address1: string;
+  address2: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  country: string;
+}
+
+export interface FileEntityDTO {
+  fileId: number;
+  uri: string;
+  fileType?: FileType;
+}
