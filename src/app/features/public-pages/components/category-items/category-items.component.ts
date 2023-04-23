b43599e7e7d8803225b26ebecae9c34638eb20de
crisnaxtha <krishnashrestha49@gmail.com>
2023-04-23 15:03:34 -0500

    Worked on Category on Home Page

--- src/app/features/public-pages/components/category-items/category-items.component.ts
+++ src/app/features/public-pages/components/category-items/category-items.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./category-items.component.scss']
 })
 export class CategoryItemsComponent {
+  categories = [
+    {
+      name: 'Books',
+      image: 'https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=650&amp;w=940',
+      link: '/books'
+    },
+    {
+      name: 'Music',
+      image: 'https://picsum.photos/3264/1836?random=1',
+      link: '/music'
+    },
+    {
+      name: 'Movies',
+      image: 'https://picsum.photos/3264/1836?random=2',
+      link: '/movies'
+    }
+  ];
+
+
+
+
 
 }
